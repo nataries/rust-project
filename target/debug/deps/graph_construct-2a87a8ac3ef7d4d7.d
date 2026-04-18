addd94e39ce74ce9fcf45a4924b40f409ec7b93f
nataries <natalizastavnaya@icloud.com>
2026-04-18 16:48:23 +0300

    Initial commit: create Rust project with Cargo

---
+++ target/debug/deps/graph_construct-2a87a8ac3ef7d4d7.d
@@ -0,0 +1,5 @@
+C:\Users\Partner34.com\Documents\graph_construct\target\debug\deps\graph_construct-2a87a8ac3ef7d4d7.d: src\main.rs
+
+C:\Users\Partner34.com\Documents\graph_construct\target\debug\deps\libgraph_construct-2a87a8ac3ef7d4d7.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/graph_construct-822c42cb62e30c45.d
@@ -0,0 +1,5 @@
+C:\Users\Partner34.com\Documents\graph_construct\target\debug\deps\graph_construct-822c42cb62e30c45.d: src\main.rs
+
+C:\Users\Partner34.com\Documents\graph_construct\target\debug\deps\libgraph_construct-822c42cb62e30c45.rmeta: src\main.rs
+
+src\main.rs:
