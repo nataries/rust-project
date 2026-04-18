f41bd1b23d5d394ce5a2195c72d8a04bd9652dbf
nataries <natalizastavnaya@icloud.com>
2026-04-18 17:02:25 +0300

    Add Graph struct

--- src/graph.rs
+++ src/graph.rs
@@ -37,3 +37,66 @@ pub struct Graph {
     pub nodes: HashMap<String, Node>,
     pub edges: Vec<Edge>,
 }
+
+impl Graph {
+    pub fn new() -> Self {
+        Self {
+            nodes: HashMap::new(),
+            edges: Vec::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, node: Node) -> Result<(), String> {
+        if self.nodes.contains_key(&node.id) {
+            return Err(format!("Узел {} уже существует", node.id));
+        }
+        self.nodes.insert(node.id.clone(), node);
+        Ok(())
+    }
+
+    pub fn add_edge(&mut self, edge: Edge) -> Result<(), String> {
+        if !self.nodes.contains_key(&edge.from) {
+            return Err(format!("Узел {} не существует", edge.from));
+        }
+        if !self.nodes.contains_key(&edge.to) {
+            return Err(format!("Узел {} не существует", edge.to));
+        }
+        self.edges.push(edge);
+        Ok(())
+    }
+
+    pub fn get_start_nodes(&self) -> Vec<String> {
+        let mut has_incoming = std::collections::HashSet::new();
+        
+        for edge in &self.edges {
+            has_incoming.insert(edge.to.clone());
+        }
+        
+        for node in self.nodes.values() {
+            if node.node_type == NodeType::Branch {
+                for branch in &node.branches {
+                    has_incoming.insert(branch.target.clone());
+                }
+            }
+        }
+        
+        self.nodes.keys()
+            .filter(|id| !has_incoming.contains(*id))
+            .cloned()
+            .collect()
+    }
+
+    pub fn get_end_nodes(&self) -> Vec<String> {
+        self.nodes.iter()
+            .filter(|(_, node)| node.node_type == NodeType::End)
+            .map(|(id, _)| id.clone())
+            .collect()
+    }
+}
+
+impl Default for Graph {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
